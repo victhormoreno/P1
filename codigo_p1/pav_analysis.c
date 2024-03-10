a5c865493578284bbb3477123e8fdb7fe62b1d9b
victor.moreno <victor.moreno.borras@estudiantat.upc.edu>
2024-03-10 22:46:04 +0100

    documentació pav_analysis

--- codigo_p1/pav_analysis.c
+++ codigo_p1/pav_analysis.c
@@ -15,6 +15,33 @@ float compute_power(const float *x, unsigned int N) {
     return log10(P/sum) * 10;
 }
 
+
+float compute_power_with_window(const float *x, unsigned int N) {
+    float P = 1e-12;
+
+    float TLong = 0.02, TDesp = 0.01;
+    int M = TDesp / TLong;  
+    int num_blocks = (N - M) / M;
+    
+    float w[N];
+    finestra_hamming(w, N);
+
+
+    for (int i = 0; i < num_blocks; ++i) {
+        int start_index = i * M;
+        for (int n = 0; n < N; ++n) {
+            P += x[start_index + n] * w[n] * x[start_index + n] * w[n];
+        }
+    }
+
+    float suma = 0.0;
+    for (int n = 0; n < N; ++n) {
+        suma += w[n] * w[n];
+    }
+
+    return 10 * log10(P / suma);
+}
+
 void finestra_hamming(float *w, unsigned int N) {
     for (int n = 0; n < N; ++n) {
         w[n] = 0.53836 - 0.46164 * cos(2.0 * M_PI * n / (N - 1));
--- codigo_p1/pav_analysis.h
+++ codigo_p1/pav_analysis.h
@@ -1,9 +1,40 @@
 #ifndef PAV_ANALYSIS_H
 #define PAV_ANALYSIS_H
 
+/**
+ * @brief Calcula la potencia de una señal.
+ *
+ * @param x Puntero a la señal de entrada.
+ * @param N Número de muestras en la señal.
+ * @return La potencia de la señal en decibelios (dB).
+ */
 float compute_power(const float *x, unsigned int N);
+
+/**
+ * @brief Calcula la amplitud media de una señal.
+ *
+ * @param x Puntero a la señal de entrada.
+ * @param N Número de muestras en la señal.
+ * @return La amplitud media de la señal.
+ */
 float compute_am(const float *x, unsigned int N);
+
+/**
+ * @brief Calcula la tasa de cruce por cero (Zero Crossing Rate) de una señal.
+ *
+ * @param x Puntero a la señal de entrada.
+ * @param N Número de muestras en la señal.
+ * @param fm Frecuencia de muestreo de la señal.
+ * @return La tasa de cruce por cero de la señal.
+ */
 float compute_zcr(const float *x, unsigned int N, float fm);
+
+/**
+ * @brief Crea la ventana de Hamming.
+ *
+ * @param w Puntero a la salida de la ventana de Hamming.
+ * @param N Número de muestras en la señal.
+ */
 void finestra_hamming(float *w, unsigned int N);
 
-#endif	/* PAV_ANALYSIS_H	*/
+#endif /* PAV_ANALYSIS_H */
